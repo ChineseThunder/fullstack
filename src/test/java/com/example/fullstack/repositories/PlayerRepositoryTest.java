b5988775616fa7c96178d5367d2bf4d1eacb70db
ChineseThunder <simo09j9@stud.kea.dk>
2022-08-25 15:18:11 +0200

    fejl

---
+++ src/test/java/com/example/fullstack/repositories/PlayerRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.example.fullstack.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerRepositoryTest {
+
+    @Test
+    void fail(){
+        assertEquals(1, 0);
+    }
+
+}
